74afca57879ed55881838caf0663758a43fdca56
Dharahasini <101558960+Dharahasini@users.noreply.github.com>
2023-10-17 10:50:51 +0530

    Update pangram or not.java

--- pangram or not.java
+++ pangram or not.java
@@ -17,22 +17,14 @@ public class PangramChecker {
     }
 
     public static boolean isPangram(String input) {
-        // Convert the input to lowercase for case insensitivity
         input = input.toLowerCase();
-
-        // Use a set to store unique characters from A to Z
         Set<Character> alphabetSet = new HashSet<>();
-        
-        // Iterate through the input and add alphabets to the set
         for (int i = 0; i < input.length(); i++) {
             char ch = input.charAt(i);
             if (ch >= 'a' && ch <= 'z') {
                 alphabetSet.add(ch);
             }
         }
-        
-        // Check if the set contains all 26 alphabets
         return alphabetSet.size() == 26;
     }
 }
-
