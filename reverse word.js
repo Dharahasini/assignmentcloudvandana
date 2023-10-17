1c955c361de349294d2d4cb451d32e2ddef230f9
Dharahasini <101558960+Dharahasini@users.noreply.github.com>
2023-10-17 11:07:35 +0530

    Update reverse word.js

--- reverse word.js
+++ reverse word.js
@@ -1,31 +1,12 @@
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
-
-public class PangramChecker {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a sentence: ");
-        String input = scanner.nextLine();
-        scanner.close();
-
-        if (isPangram(input)) {
-            System.out.println("The input is a pangram.");
-        } else {
-            System.out.println("The input is not a pangram.");
-        }
-    }
-
-    public static boolean isPangram(String input) {
-        input = input.toLowerCase();
-        Set<Character> alphabetSet = new HashSet<>();
-        for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            if (ch >= 'a' && ch <= 'z') {
-                alphabetSet.add(ch);
-            }
-        }
-        return alphabetSet.size() == 26;
-    }
+function reverseWordsInSentence(sentence) {
+    const words = sentence.split(' ');
+    const reversedWords = words.map(word => reverseWord(word));
+    const reversedSentence = reversedWords.join(' ');
+    return reversedSentence;
 }
-
+function reverseWord(word) {
+    return word.split('').reverse().join('');
+}
+const inputSentence = "hi hasini";
+const reversedSentence = reverseWordsInSentence(inputSentence);
+console.log(reversedSentence);
